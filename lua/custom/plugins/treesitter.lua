71cfeaee332340d1ac99350e25fd9a7b8eb98e6f
mle98 <matthias.lerch@protonmail.com>
2025-02-23 14:20:01 +0100

    add treesitter context for sticky method header

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -24,4 +24,7 @@ return {
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+  },
 }
