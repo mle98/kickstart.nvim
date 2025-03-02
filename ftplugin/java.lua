3708c5e2b2efac8e0a0ceb3331e15f7e38f335c9
mle98 <matthias.lerch@protonmail.com>
2025-03-02 17:07:16 +0100

    change jdk version

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,6 @@
 local config = {
 
-  cmd = { '/home/mat/.local/share/nvim/mason/packages/jdtls/bin/jdtls', '--java-executable', '/usr/lib/jvm/jdk-23.0.1/bin/java' },
+  cmd = { '/home/mat/.local/share/nvim/mason/packages/jdtls/bin/jdtls', '--java-executable', '/usr/lib/jvm/jdk-21.0.2/bin/java' },
   root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
   settings = {
     java = {},
