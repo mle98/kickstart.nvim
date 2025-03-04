7a79b11ebdc17d2a1dba47be82c9446cab0f5721
mle98 <matthias.lerch@protonmail.com>
2025-03-04 20:52:27 +0100

    introduce harpoon and noice

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		'ThePrimeagen/harpoon',
+		branch = 'harpoon2',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+
+		config = function () 
+			local harpoon = require 'harpoon'
+
+			-- REQUIRED
+			harpoon:setup()
+			-- REQUIRED
+
+			vim.keymap.set("n", "<M-a>", function() harpoon:list():add() end)
+			vim.keymap.set('n', '<M-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+			vim.keymap.set('n', '<M-j>', function() harpoon:list():select(1) end)
+			vim.keymap.set('n', '<M-k>', function() harpoon:list():select(2) end)
+			vim.keymap.set('n', '<M-l>', function() harpoon:list():select(3) end)
+			vim.keymap.set('n', '<M-;>', function() harpoon:list():select(4) end)
+		end
+	},
+}
