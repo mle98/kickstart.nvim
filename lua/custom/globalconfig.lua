3eb5009cec1e71954f104f35104bf54f02a04715
mle98 <matthias.lerch@protonmail.com>
2025-03-04 20:52:06 +0100

    general QOL

---
+++ lua/custom/globalconfig.lua
@@ -0,0 +1,23 @@
+-- Set spaces as tabs with a tab size of 4 spaces
+vim.o.expandtab = true  -- Use spaces instead of tabs
+vim.o.shiftwidth = 4     -- Number of spaces for each indentation level
+vim.o.tabstop = 4        -- Number of spaces per tab
+vim.o.softtabstop = 4    -- Number of spaces when pressing Tab
+
+-- Remap <Leader><C-a> to select all text in normal mode
+vim.api.nvim_set_keymap('n', '<Leader><C-a>', 'ggVG', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
